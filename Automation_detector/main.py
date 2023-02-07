18402e6f4d8d31b4a55c40a74819e1d831087a0a
Rahul <rahul.kumark72@gmail.com>
2023-02-08 03:00:52 +0530

    "spiek story "

--- Automation_detector/main.py
+++ Automation_detector/main.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
                 print('Sorry binod is not available today')
     print("****************Summary of Coding***************************************")
     print("Pushpa I hate u yar")
+    print("Hey Love I miss you buddy so much")
     print(nbinod)
     print("Hello world")
     print("All test passed")
