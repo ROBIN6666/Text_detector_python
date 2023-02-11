9adf440be5dfe705c4c6eba0fe23c56fdd6d650d
Rahul <rahul.kumark72@gmail.com>
2023-02-12 00:28:06 +0530

    Merge branch 'x7m4k6'

--- Automation_detector/main.py
+++ Automation_detector/main.py
@@ -1,25 +1,26 @@
-import os 
+import os
+
+
 def search(filename):
-    with open(filename , 'r') as f:
-          file_read=f.read()
+    with open(filename, 'r') as f:
+        file_read = f.read()
     if 'binod' in file_read.lower():
         return True
     else:
         return False
 
 
-
 if __name__ == "__main__":
     content = os.listdir()
-    nbinod=0
-    flag=0
+    nbinod = 0
+    flag = 0
     for item in content:
         if item.endswith('txt'):
             print('Detecting Binod:{}'.format(item))
-            flag=search(item)
-            if(flag==1):
+            flag = search(item)
+            if (flag == 1):
                 print('We found Binod in this textfile:{}'.format(item))
-                nbinod+=1
+                nbinod += 1
             else:
                 print('Sorry binod is not available today')
     print("****************Summary of Coding***************************************")
